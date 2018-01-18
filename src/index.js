fcc2dd22fc6846a046d380c61ea0687ab55c6ff8
Jinho Chung <jinho@radishfiction.com>
2018-01-18 11:09:05 +0900

    remove sanitize option

--- src/index.js
+++ src/index.js
@@ -12,25 +12,8 @@ var snakeCaseLib = require('snake-case')
  *
  */
 
-// mongoose sanitizer example
-var mongooseObjectSanitizer = (node) => {
-  var needToMakeObjectFirst = ['model', 'EmbeddedDocument']
-  var needToOmitFields = ['_id', '__v']
-
-  if (!_.isEmpty(node) && typeof node === 'object' && node.constructor && needToMakeObjectFirst.includes(node.constructor.name)) {
-    return _.omit(node.toObject(), needToOmitFields)
-  } else {
-    return node
-  }
-}
-
 var options = {
-  ignoreClass: [], // e.g. ['Date']
-
-  // for Mongoose Object
-  objectSanitizer: (node) => {
-    return node
-  }
+  ignoreClass: [] // e.g. ['Date']
 }
 
 var option = (inputOptions) => {
@@ -40,35 +23,6 @@ var option = (inputOptions) => {
 var resolveContextOptions = (oneOffOptions) => {
   oneOffOptions = _.assignIn(oneOffOptions, options)
   return oneOffOptions
-}
-
-var sanitize = (node, contextOptions) => {
-  if (!node) {
-    return node
-  }
-
-  if (!contextOptions) {
-    return node
-  }
-
-  if (!contextOptions) {
-    throw new Error('Option must have values')
-  } else {
-    var objectSanitizer = contextOptions.objectSanitizer
-    if (!objectSanitizer) {
-      return node
-    } else if (typeof objectSanitizer === 'function') {
-      return objectSanitizer(node)
-    } else if (typeof objectSanitizer === 'object') {
-      if (node.constructor && node.constructor.name) {
-        var constructorName = node.constructor.name
-        if (objectSanitizer.hasOwnProperty(constructorName) && typeof objectSanitizer[constructorName] === 'function') {
-          return objectSanitizer[constructorName](node)
-        }
-      }
-    }
-  }
-  return node
 }
 
 var camelcase = (node, oneOffOptions) => {
@@ -124,10 +78,9 @@ var transformArray = (nodes, f, contextOptions) => {
 }
 
 var transformObject = (node, f, contextOptions) => {
-  var obj = sanitize(node, contextOptions)
   var newObj = {}
 
-  _.forIn(obj, (value, key) => {
+  _.forIn(node, (value, key) => {
     var transformedKey = f(key)
 
     if (isArray(value)) {
